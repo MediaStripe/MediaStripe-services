dc7338ca06936fc5d43f86cfc392d85732945fb5
Takiguchi <florian.thierry72@gmail.com>
2016-09-17 17:22:34 +0200

    Ajout de la méthode FichierService.findByPath()

--- src/main/java/com/imie/services/impl/FichierService.java
+++ src/main/java/com/imie/services/impl/FichierService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
 
 import com.imie.entities.Fichier;
 import com.imie.services.AbstractPersistenceService;
@@ -52,4 +54,24 @@ public class FichierService extends AbstractPersistenceService<Fichier> {
 		em.getTransaction().commit();
 	}
 	
+	/**
+	 * Retourne le fichier correspondant au chemin passé en paramètres.
+	 * @param cheminfichier Le chemin/nom du fichier.
+	 * @return Le fichier correspondant au chemin spécifié en paramètres.
+	 */
+	public Fichier findByPath(final String cheminfichier) {
+		final Query query = em.createNamedQuery("Fichier.findByPath");
+
+		query.setParameter("cheminfichier", cheminfichier);
+		
+		Fichier fichier = null;
+		try {
+			fichier = (Fichier) query.getSingleResult();
+		} catch (final PersistenceException e) {
+			System.err.println(new StringBuilder("Aucun fichier trouvé pour l'adresse suivante : ").append(cheminfichier).toString());
+			throw e;
+		}
+		
+		return fichier;
+	}
 }
